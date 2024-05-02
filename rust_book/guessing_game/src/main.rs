f4954a52fd1ee47770f43e2a4dbe1779ecc3e047
leuname592 <leuname592@hotmail.com>
2024-05-01 20:55:53 -0600

    Adding a directory to all the exercises of rust book

---
+++ rust_book/guessing_game/src/main.rs
@@ -0,0 +1,13 @@
+use std::io;
+use rand::Rng;
+fn main() {
+    println!("Guess the number!");
+    println!("Enter your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is {secret_number}");
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed: {guess}");
+}
